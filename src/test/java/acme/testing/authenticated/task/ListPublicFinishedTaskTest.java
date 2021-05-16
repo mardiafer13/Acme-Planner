9aa96bc8a9f00ef3022106889143e13adfa1c1af
Diego <scoobydoobydoo113@gmail.com>
2021-05-17 01:22:03 +0200

    ejemplo del primer test

--- src/test/java/acme/testing/authenticated/task/ListPublicFinishedTaskTest.java
+++ src/test/java/acme/testing/authenticated/task/ListPublicFinishedTaskTest.java
@@ -1,49 +1,102 @@
 package acme.testing.authenticated.task;
 
-import org.junit.jupiter.api.BeforeAll;
+import java.util.Date;
 
-import acme.framework.helpers.StringHelper;
-import acme.testing.AcmeTest;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
-public abstract class ListPublicFinishedTaskTest extends AcmeTest {
+import acme.testing.AcmePlannerTest;
 
-	@Override
-	@BeforeAll
-	public void beforeAll() {
-		super.beforeAll();
+public abstract class ListPublicFinishedTaskTest extends AcmePlannerTest {
 
-		super.setBaseCamp("http", "localhost", "8080", "/Acme-Planner", "/master/welcome", "?language=en&debug=true");
-		super.setAutoPausing(false);
-
-		this.navigateHome();
-		this.signIn("manager1", "manager1");
-		super.clickOnMenu("Authenticated", "List finished tasks");
-		super.checkAlertExists(true);
-		this.signOut();
-	}
-
-	//Business methods --------------------
-
-	protected void signIn(final String username, final String password) {
-		assert !StringHelper.isBlank(username);
-		assert !StringHelper.isBlank(password);
-
-		super.navigateHome();
-		super.clickOnMenu("Sign in", null);
-		super.fillInputBoxIn("username", username);
-		super.fillInputBoxIn("password", password);
-		super.fillInputBoxIn("remember", "true");
-		super.clickOnSubmitButton("Sign in");
-		super.checkSimplePath("/master/welcome");
-		super.checkLinkExists("Account");
-
+	
+	//Lifecycle management
+	
+	//Test cases
+	
+	@ParameterizedTest
+	@CsvFileSource()
+	@Order(10)
+	public void createPositive(final int recordIndex,final String title, final Date periodInitial, final Date periodFinal, final String description, final String link, final Boolean isPublic) {
+		//Con que usuario entramos
+		super.signIn(this.contextHome, this.baseUrl);
+		
+		//Donde debe entrar para hacer la comprobacion
+		super.clickOnMenu("Authenticate", "List all tasks");
+		
+//Esto es si se necesita poner algo en el input
+//		super.fillInputBoxIn("title", title);
+////		super.fillInputBoxIn("periodInitial", periodInitial);
+////		super.fillInputBoxIn("periodFinal", periodFinal);
+//		super.fillInputBoxIn("description", description);
+//		super.fillInputBoxIn("link", link);
+////		super.fillInputBoxIn("isPublic", isPublic);
+		
+		//Comprobar en que posicion del listado se encuentra el atributo
+		super.checkColumnHasValue(recordIndex, 1, title);
+		
+		//Comprobar el valor del atributo
+		super.checkInputBoxHasValue("title", title);
+		
+		super.signOut();
 	}
 	
-	protected void signOut() {
-		super.navigateHome();
-		super.clickOnMenu("Sign out", null);
-		super.checkSimplePath("/master/welcome");
-	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+//	@Override
+//	@BeforeAll
+//	public void beforeAll() {
+//		super.beforeAll();
+//
+//		super.setBaseCamp("http", "localhost", "8080", "/Acme-Planner", "/master/welcome", "?language=en&debug=true");
+//		super.setAutoPausing(false);
+//
+//		this.navigateHome();
+//		this.signIn("manager1", "manager1");
+//		super.clickOnMenu("Authenticated", "List finished tasks");
+//		super.checkAlertExists(true);
+//		this.signOut();
+//	}
+//
+//	//Business methods --------------------
+//
+//	protected void signIn(final String username, final String password) {
+//		assert !StringHelper.isBlank(username);
+//		assert !StringHelper.isBlank(password);
+//
+//		super.navigateHome();
+//		super.clickOnMenu("Sign in", null);
+//		super.fillInputBoxIn("username", username);
+//		super.fillInputBoxIn("password", password);
+//		super.fillInputBoxIn("remember", "true");
+//		super.clickOnSubmitButton("Sign in");
+//		super.checkSimplePath("/master/welcome");
+//		super.checkLinkExists("Account");
+//
+//	}
+//	
+//	protected void signOut() {
+//		super.navigateHome();
+//		super.clickOnMenu("Sign out", null);
+//		super.checkSimplePath("/master/welcome");
+//	}
 
 }
 
