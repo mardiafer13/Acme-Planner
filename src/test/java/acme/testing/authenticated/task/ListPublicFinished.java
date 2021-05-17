6e6c9b2b01b5e8878a604176678cdb43be0b5346
César <cesgaldor@alum.us.es>
2021-05-17 18:24:27 +0200

    Test authenticated list public and finished tasks done

--- src/test/java/acme/testing/authenticated/task/ListPublicFinished.java
+++ src/test/java/acme/testing/authenticated/task/ListPublicFinished.java
@@ -7,16 +7,14 @@ import org.junit.jupiter.params.provider.CsvFileSource;
 import acme.testing.AcmePlannerTest;
 
 
-public class ListFinish extends AcmePlannerTest{
-
-	//Lifecycle management------------------
+public class ListPublicFinished extends AcmePlannerTest{
 	
 		//Test cases----------------------
 		
 		@ParameterizedTest
 		@CsvFileSource(resources = "/authenticated/task/listFinishPublicTasks.csv", encoding = "utf-8", numLinesToSkip = 1)
 		@Order(10)
-		public void ListPublicFinished(final int recordIndex,final String title, final String description,final String periodInitial, final String periodFinal, final String workload, final String link) {
+		public void ListPublicFinishedTasks(final int recordIndex,final String title, final String description,final String periodInitial, final String periodFinal, final String workload, final String link) {
 			//Con que usuario entramos
 			super.signIn("manager1", "manager1");
 			
@@ -32,12 +30,9 @@ public class ListFinish extends AcmePlannerTest{
 			//super.fillInputBoxIn("isPublic", isPublic);
 			
 			//Comprobar en que posicion del listado se encuentra el atributo
-			super.checkColumnHasValue(recordIndex, 0, title);
-			super.checkColumnHasValue(recordIndex, 1, description);
-			super.checkColumnHasValue(recordIndex, 2, periodInitial);
-			super.checkColumnHasValue(recordIndex, 3, periodFinal);
-			super.checkColumnHasValue(recordIndex, 4, workload);
-			super.checkColumnHasValue(recordIndex, 5, link);
+			super.checkColumnHasValue(recordIndex, 0, periodInitial);
+			super.checkColumnHasValue(recordIndex, 1, periodFinal);
+			super.checkColumnHasValue(recordIndex, 2, title);
 			
 			//Clica
 			super.clickOnListingRecord(recordIndex);
@@ -50,7 +45,7 @@ public class ListFinish extends AcmePlannerTest{
 			super.checkInputBoxHasValue("workloadInHours", workload);
 			super.checkInputBoxHasValue("link", link);
 			
-			
 			super.signOut();
+			
 		}
 }
