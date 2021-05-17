ab6208eee7c24646ffb2f37bde49f4eb416d23f4
Diego <scoobydoobydoo113@gmail.com>
2021-05-17 14:50:43 +0200

    primer test de task

--- src/test/java/acme/testing/authenticated/task/ListPublicFinishedTaskTest.java
+++ src/test/java/acme/testing/authenticated/task/ListPublicFinishedTaskTest.java
@@ -1,7 +1,5 @@
 package acme.testing.authenticated.task;
 
-import java.util.Date;
-
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
@@ -11,92 +9,46 @@ import acme.testing.AcmePlannerTest;
 public abstract class ListPublicFinishedTaskTest extends AcmePlannerTest {
 
 	
-	//Lifecycle management
+	//Lifecycle management------------------
 	
-	//Test cases
+	//Test cases----------------------
 	
 	@ParameterizedTest
-	@CsvFileSource()
+	@CsvFileSource(resources = "/task/list.csv", encoding = "utf-8", numLinesToSkip = 1)
 	@Order(10)
-	public void createPositive(final int recordIndex,final String title, final Date periodInitial, final Date periodFinal, final String description, final String link, final Boolean isPublic) {
+	public void createPositive(final int recordIndex,final String title, final String periodInitial, final String periodFinal, final String description, final String link, final String workload) {
 		//Con que usuario entramos
-		super.signIn(this.contextHome, this.baseUrl);
+		super.signIn("manager1", "manager1");
 		
 		//Donde debe entrar para hacer la comprobacion
-		super.clickOnMenu("Authenticate", "List all tasks");
+		super.clickOnMenu("Authenticated", "List finished tasks");
 		
 //Esto es si se necesita poner algo en el input
 //		super.fillInputBoxIn("title", title);
-////		super.fillInputBoxIn("periodInitial", periodInitial);
-////		super.fillInputBoxIn("periodFinal", periodFinal);
+//		super.fillInputBoxIn("periodInitial", periodInitial);
+//		super.fillInputBoxIn("periodFinal", periodFinal);
 //		super.fillInputBoxIn("description", description);
 //		super.fillInputBoxIn("link", link);
-////		super.fillInputBoxIn("isPublic", isPublic);
+//		super.fillInputBoxIn("isPublic", isPublic);
 		
 		//Comprobar en que posicion del listado se encuentra el atributo
-		super.checkColumnHasValue(recordIndex, 1, title);
+		super.checkColumnHasValue(recordIndex, 0, periodInitial);
+		super.checkColumnHasValue(recordIndex, 1, periodFinal);
+		super.checkColumnHasValue(recordIndex, 2, title);
+		
+		//Clica
+		super.clickOnListingRecord(recordIndex);
 		
 		//Comprobar el valor del atributo
 		super.checkInputBoxHasValue("title", title);
+		super.checkInputBoxHasValue("description", description);
+		super.checkInputBoxHasValue("begin", periodInitial);
+		super.checkInputBoxHasValue("end", periodFinal);
+		super.checkInputBoxHasValue("workload", workload);
+		super.checkInputBoxHasValue("link", link);
+		
 		
 		super.signOut();
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-//	@Override
-//	@BeforeAll
-//	public void beforeAll() {
-//		super.beforeAll();
-//
-//		super.setBaseCamp("http", "localhost", "8080", "/Acme-Planner", "/master/welcome", "?language=en&debug=true");
-//		super.setAutoPausing(false);
-//
-//		this.navigateHome();
-//		this.signIn("manager1", "manager1");
-//		super.clickOnMenu("Authenticated", "List finished tasks");
-//		super.checkAlertExists(true);
-//		this.signOut();
-//	}
-//
-//	//Business methods --------------------
-//
-//	protected void signIn(final String username, final String password) {
-//		assert !StringHelper.isBlank(username);
-//		assert !StringHelper.isBlank(password);
-//
-//		super.navigateHome();
-//		super.clickOnMenu("Sign in", null);
-//		super.fillInputBoxIn("username", username);
-//		super.fillInputBoxIn("password", password);
-//		super.fillInputBoxIn("remember", "true");
-//		super.clickOnSubmitButton("Sign in");
-//		super.checkSimplePath("/master/welcome");
-//		super.checkLinkExists("Account");
-//
-//	}
-//	
-//	protected void signOut() {
-//		super.navigateHome();
-//		super.clickOnMenu("Sign out", null);
-//		super.checkSimplePath("/master/welcome");
-//	}
-
 }
 
