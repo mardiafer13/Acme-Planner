6283e427f8b0d2466af87bc6ed4d20305d51beaf
mardiafer13 <mardiafer13@alum.us.es>
2021-05-17 10:53:07 +0200

    task12

--- src/test/java/acme/testing/anonymous/AnonymousShoutCreateTest.java
+++ src/test/java/acme/testing/anonymous/AnonymousShoutCreateTest.java
@@ -11,14 +11,24 @@ public class AnonymousShoutCreateTest extends AcmePlannerTest {
 
 	
 	@ParameterizedTest
-	@CsvFileSource(resources = "/anonymous/shout/create.csv", encoding = "utf-8", numLinesToSkip = 1)
+	@CsvFileSource(resources = "/anonymous/shout/create-positive.csv", encoding = "utf-8", numLinesToSkip = 1)
 	@Order(10)
 	public void createPositive(final int recodIndex, final String author, final String text, final String info) {
-		this.signOut();
+		
 		super.clickOnMenu("Anonymous", "Create a shout");
 		super.fillInputBoxIn("author", author);
 		super.fillInputBoxIn("text", text);
 		super.fillInputBoxIn("info", info);
 		super.clickOnSubmitButton("Shout!");
 	}
+	
+	@ParameterizedTest
+	@CsvFileSource(resources = "/anonymous/shout/create-negative.csv", encoding = "utf-8", numLinesToSkip = 1)
+	@Order(10)
+	public void createNegative(final int recodIndex, final String author, final String text, final String info) {
+		
+		super.clickOnMenu("Anonymous", "Create a shout");
+		super.fillInputBoxIn("author", author);
+			
+		}
 }
