c0e235dc40b1ff747ab8312e071ef97f65f2303b
javgrogom <javgrogom@alum.us.es>
2021-05-22 19:14:53 +0200

    Avanzado en AuthenticatedTaskShowService

--- src/test/java/acme/testing/authenticated/AuthenticatedTaskShowService.java
+++ src/test/java/acme/testing/authenticated/AuthenticatedTaskShowService.java
@@ -12,19 +12,22 @@ public class AuthenticatedTaskShowService extends AcmePlannerTest{
 	@ParameterizedTest
 	@CsvFileSource(resources = "/authenticated/task/listTaskAuthenticated.csv", encoding = "utf-8", numLinesToSkip = 1)
 	@Order(10)
-	public void taskShowServiceTest(final int recordIndex,final String title, final String description,final String periodInitial, final String periodFinal, final String workload, final String link) {
+	public void taskShowServiceTest(final int recordIndex,final String title,final String description, final String periodInitial, final String periodFinal ,final String workloadInHours, final String Link) {
 		super.signIn("manager1", "manager1");
 		super.clickOnMenu("Manager", "List my tasks");
 		
-		super.checkColumnHasValue(recordIndex, 0, title);
-		super.checkColumnHasValue(recordIndex, 1, description);
-		super.checkColumnHasValue(recordIndex, 2, periodInitial);
-		super.checkColumnHasValue(recordIndex, 3, periodFinal);
-		super.checkColumnHasValue(recordIndex, 4, workload);
-		super.checkColumnHasValue(recordIndex, 5, link);
+		super.checkColumnHasValue(recordIndex, 0, periodInitial);
+		super.checkColumnHasValue(recordIndex, 1, periodFinal);
+		super.checkColumnHasValue(recordIndex, 2, title);
+
+		super.clickOnListingRecord(recordIndex);
 		
-		
-		
+		super.checkInputBoxHasValue("title", title);
+		super.checkInputBoxHasValue("description", description);
+		super.checkInputBoxHasValue("periodInitial", periodInitial);
+		super.checkInputBoxHasValue("periodFinal", periodFinal);
+		super.checkInputBoxHasValue("workloadInHours", workloadInHours);
+		super.checkInputBoxHasValue("link", Link);
 		
 		super.signOut();
 	}
