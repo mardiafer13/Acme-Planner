cc5d73db119daafa2a1687ec38ae6245c5a84e12
Diego <scoobydoobydoo113@gmail.com>
2021-05-25 15:21:14 +0200

    delete negative test

--- src/test/java/acme/testing/manager/task/ManagerTaskDelete.java
+++ src/test/java/acme/testing/manager/task/ManagerTaskDelete.java
@@ -44,4 +44,90 @@ public class ManagerTaskDelete extends AcmePlannerTest {
 		super.signOut();
 
 	}
+	
+	
+	//Test delete negativo de anonymous
+	@ParameterizedTest
+	@CsvFileSource(resources = "/manager/task/deleteNegative.csv", encoding = "utf-8", numLinesToSkip = 1)
+	@Order(20)
+	public void deleteTasksNegativeAnonymous(final int recordIndex, final String title, final String description, final String periodInitial, final String periodFinal, final String workloadInHours, final String link, final Boolean isPublic) {
+
+		// Clica en el menú para acceder a las tareas del manager
+		this.driver.get("localhost:8080/Acme-Planner/manager/task/show?id=20");
+		
+		super.checkPanicExists();
+
+	}
+	
+	//Test delete negativo de administrador
+	@ParameterizedTest
+	@CsvFileSource(resources = "/manager/task/deleteNegative.csv", encoding = "utf-8", numLinesToSkip = 1)
+	@Order(20)
+	public void deleteTasksNegativeAdministrator(final int recordIndex, final String title, final String description, final String periodInitial, final String periodFinal, final String workloadInHours, final String link, final Boolean isPublic) {
+
+		// Se logea con el usuario manager
+		super.signIn("administrator", "administrator");
+
+		// Clica en el menú para acceder a las tareas del manager
+		this.driver.get("localhost:8080/Acme-Planner/manager/task/show?id=20");
+		
+		super.checkPanicExists();
+		
+		super.signOut();
+
+	}
+	
+	//Test delete negativo de employer
+	@ParameterizedTest
+	@CsvFileSource(resources = "/manager/task/deleteNegative.csv", encoding = "utf-8", numLinesToSkip = 1)
+	@Order(20)
+	public void deleteTasksNegativeEmployer(final int recordIndex, final String title, final String description, final String periodInitial, final String periodFinal, final String workloadInHours, final String link, final Boolean isPublic) {
+
+		// Se logea con el usuario manager
+		super.signIn("employer1", "employer1");
+
+		// Clica en el menú para acceder a las tareas del manager
+		this.driver.get("localhost:8080/Acme-Planner/manager/task/show?id=20");
+		
+		super.checkPanicExists();
+
+		super.signOut();
+
+	}
+	
+	//Test delete negativo de worker
+	@ParameterizedTest
+	@CsvFileSource(resources = "/manager/task/deleteNegative.csv", encoding = "utf-8", numLinesToSkip = 1)
+	@Order(20)
+	public void deleteTasksNegativeWorker(final int recordIndex, final String title, final String description, final String periodInitial, final String periodFinal, final String workloadInHours, final String link, final Boolean isPublic) {
+
+		// Se logea con el usuario manager
+		super.signIn("worker1", "worker1");
+
+		// Clica en el menú para acceder a las tareas del manager
+		this.driver.get("localhost:8080/Acme-Planner/manager/task/show?id=20");
+		
+		super.checkPanicExists();
+		
+		super.signOut();
+	}
+	
+	//Test delete negativo de otro manager
+	@ParameterizedTest
+	@CsvFileSource(resources = "/manager/task/deleteNegative.csv", encoding = "utf-8", numLinesToSkip = 1)
+	@Order(20)
+	public void deleteTasksNegativeManager(final int recordIndex, final String title, final String description, final String periodInitial, final String periodFinal, final String workloadInHours, final String link, final Boolean isPublic) {
+
+		// Se logea con el usuario manager
+		super.signIn("manager2", "manager2");
+
+		// Clica en el menú para acceder a las tareas del manager
+		this.driver.get("localhost:8080/Acme-Planner/manager/task/show?id=20");
+		
+		super.checkPanicExists();
+		
+		super.signOut();
+	}
+	
+	
 }
